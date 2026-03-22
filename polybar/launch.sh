9a258b9f279821ff54455358bca2d64085cb6f54
Your Name <you@example.com>
2026-03-23 00:05:28 +0200

    remove tuff

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-# polybar-msg cmd quit
-killall -q polybar
-
-# Launch polybar
-echo "---" | tee -a /tmp/polybar1.log 
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
