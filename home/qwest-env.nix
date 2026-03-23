aabc1fa15422657dbb5303924b6a0683202c1e03
Georgi Kerkelov <qwest@kali.local>
2026-03-23 18:26:00 -0400

    change rofi config

--- home/qwest-env.nix
+++ home/qwest-env.nix
@@ -2,19 +2,19 @@ let pkgs = import <nixpkgs> {config.allowUnfree = true;}; in pkgs.buildEnv {
 	name = "qwest-env";
 	paths = with pkgs; [
 		starship        #1.  terminal Prompt bar 
-		neovim     	#2.  text editor
-		tmux       	#3.  terminal multiplexer
-		rofi       	#4.  application launcher on steroids
-		yazi       	#5.  terminal file manager
-		bat        	#6.  better cat
-		eza        	#7.  better ls
-		fd         	#8.  better find
-		ripgrep    	#9.  better grep
-		btop       	#10. terminal system monitor
-		zoxide    	#11. better cd
-		tldr       	#12. better man
-		fzf        	#13. general-purpose fuzzy finder
-		neo        	#14. matrix rain (use the numbers to change color)
+		neovim     	    #2.  text editor
+		tmux       	    #3.  terminal multiplexer
+		rofi       	    #4.  application launcher on steroids
+		yazi       	    #5.  terminal file manager
+		bat        	    #6.  better cat
+		eza        	    #7.  better ls
+		fd         	    #8.  better find
+		ripgrep    	    #9.  better grep
+		btop       	    #10. terminal system monitor
+		zoxide    	    #11. better cd
+		tldr       	    #12. better man
+		fzf        	    #13. general-purpose fuzzy finder
+		neo        	    #14. matrix rain (use the numbers to change color)
 		google-chrome   #15. browser 
 		atuin           #16. Better Shell History
 		starship        #17. terminal Prompt bar  
@@ -27,6 +27,7 @@ let pkgs = import <nixpkgs> {config.allowUnfree = true;}; in pkgs.buildEnv {
 		dust            #24. modern du
 		gum             #25. better TUI for user interaction in shell scripts
 		stow            #26. symlinc the dotfiles repo
+		fastfetch       #27. display info in a cool way in the terminal 
 	]; 
 }
 
