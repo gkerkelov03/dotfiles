fbdbd5a7e00b785cd9adb27e4b244dacc86f3fda
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-08 00:45:02 +0000

    start using caelestia shell

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -24,15 +24,15 @@
 		lazydocker      #docker TUI
 		sheldon         #zsh plugin manager
 
-        #GUI apps
-		google-chrome   #browser 
+		#GUI apps
+		mpv
 
-        #Git
+		#Git
 		lazygit         #git TUI 
-        gh              #github cli
+		gh              #github cli
 		delta           #syntax-highlighting pager for GitHub-style terminal diffs
 
-        #Modern CLI tools that replace the old ones
+		#Modern CLI tools that replace the old ones
 		bat             #better cat
 		eza             #better ls
 		fd              #better find
@@ -41,24 +41,25 @@
 		tldr            #better man
 		duf             #better df
 		dust            #better du
-        dog             #better dig
-        xh              #better curl
+		dog             #better dig
+		xh              #better curl
         
-        #Terminal visualizers
+		#Terminal visualizers
 		neo             #matrix rain (use the numbers to change color)
 		fastfetch       #display system info 
-        pipes
+		pipes
 
-        #JSON
+		#JSON
 		jless           #view json
 		jc              #turn text to json
 		gron            #flatten json (mainly for grep/awk)
 		jqp             #jq query builder
 		jq              #operate on json
 
-        #Utilities
-        ouch            #archive & unarchive
-		age				#encryption tool
+		#Utilities
+		ouch            #archive & unarchive
+		age		#encryption tool
+		mpvpaper        #set videos as wallpaper
 	];
 }
 
