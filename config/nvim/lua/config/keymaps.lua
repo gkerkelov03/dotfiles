34e612e714d04aa9234a5a3cf3f7227d7b0a587b
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-10 09:22:09 +0000

    start mainly using qutebrowser

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,24 +1,6 @@
 local keymap = vim.keymap
-
--- Exit insert mode with jj
-keymap.set("i", "jj", "<Esc>")
-
--- Map 0 to ^
-keymap.set({ "n", "x" }, "0", "^")
-
--- Open new lines without staying in insert mode
-keymap.set("n", "o", "o<Esc>")
-keymap.set("n", "O", "O<Esc>")
-
--- Center search results
-keymap.set("n", "n", "nzz")
-keymap.set("n", "N", "Nzz")
-
--- Delete all
-keymap.set("n", "<leader>da", ":%d<cr>", { desc = "Delete all" })
-
--- Copy all
-keymap.set("n", "<leader>ya", ":%y+<cr>", { desc = "Copy all" })
-
--- Paste on the line above
-keymap.set("n", "P", "kp", { desc = "Paste on line above" })
+keymap.set({ "n", "x" }, "0", "^")                                -- Map 0 to ^
+keymap.set("n", "o", "o<Esc>")                                    -- Open new lines without staying in insert mode
+keymap.set("n", "<leader>da", ":%d<cr>", { desc = "Delete all" }) -- Delete all
+keymap.set("n", "<leader>ya", ":%y+<cr>", { desc = "Copy all" })  -- Copy all
+keymap.set("n", "P", "kp", { desc = "Paste on line above" })      -- Paste on the line above
