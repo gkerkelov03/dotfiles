184637000f19779c735b4f1cb342ee1cf9260a9a
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-10 12:09:34 +0000

    move home into config/home-dir

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,34 +1,58 @@
 import os 
 
-config.load_autoconfig(False) #don't allow config outside of this file
+#Don't allow config outside of this file
+config.load_autoconfig(False) 
 
-c.url.default_page = "https://google.com/gemini"
+#Default pages to open
 c.url.start_pages = "https://google.com/gemini"
+c.url.default_page = "https://google.com/gemini"
+
+#make :open yt gosho search directly in youtube and so on...
 c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?q={}",
     "yt": "https://www.youtube.com/results?search_query={}",
     "gh": "https://github.com/search?q={}",
+    "maps": "www.google.com/maps?q={}"
 }
 
+#alias :src to :cnofig-source and so on...
 c.aliases = {
-    "ytmpv": "spawn --detach mpv --ytdl-format=best {url}",
     "src": "config-source",
     "inst": "open instagram.com"
 }
 
-c.tabs.background = True
-
+# Setup q w e to do -0.1 +0.1 =1 speed
 config.bind("q", "clear-messages ;; jseval document.querySelector('video, audio').playbackRate = (document.querySelector('video, audio').playbackRate - 0.1).toFixed(1)")
 config.bind("w", "clear-messages ;; jseval document.querySelector('video, audio').playbackRate = (document.querySelector('video, audio').playbackRate + 0.1).toFixed(1)")
 config.bind("e", "clear-messages ;; jseval document.querySelector('video, audio').playbackRate = (document.querySelector('video, audio').playbackRate = 1)")
 
+# Setup yazi as an external file picker
 c.downloads.location.directory = os.path.expanduser("~/downloads")
-c.fonts.default_family = "JetBrainsMono Nerd Font"
+c.fileselect.handler = "external"
+yazi_chooser = [
+    "foot",
+    "--", 
+    "yazi", 
+    "--chooser-file", 
+    "{}" 
+]
 
+c.fileselect.single_file.command = yazi_chooser
+c.fileselect.multiple_files.command = yazi_chooser
+c.fileselect.folder.command = yazi_chooser
+
+# Use GPU as much as possible
+c.qt.args = [
+    "enable-accelerated-video",
+    "enable-native-gpu-memory-buffers",
+    "ignore-gpu-blocklist",
+    "enable-quic",
+]
+c.qt.workarounds.disable_accelerated_2d_canvas = "never"
+
+#Options
 c.content.autoplay = False
 c.content.notifications.enabled = False
-c.content.dns_prefetch = False
-
-c.auto_save.session = True
-c.completion.height = "30%"
-
+c.session.lazy_restore = True
+c.scrolling.smooth = True
+c.completion.height = "20%"
