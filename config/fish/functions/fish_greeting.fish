5930abcfd5878f9bc02eda377a60491ed018293f
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-11 13:50:58 +0000

    start working on fish and nvm

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    echo -ne '\x1b[38;5;16m'  # Set colour to primary
+    echo -ne '\x1b[38;5;16m' # Set colour to primary
     echo '     ______           __          __  _       '
     echo '    / ____/___ ____  / /__  _____/ /_(_)___ _ '
     echo '   / /   / __ `/ _ \/ / _ \/ ___/ __/ / __ `/ '
