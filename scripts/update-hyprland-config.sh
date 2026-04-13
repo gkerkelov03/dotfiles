e3c21a537f4e6403fce966edcb4644c4963c17b3
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-13 13:14:47 +0000

    start removing nix and home-manager

---
+++ scripts/update-hyprland-config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'Installing hypr config...'
+git clone --depth 1 --no-checkout https://github.com/caelestia-dots/caelestia.git tmp
+cd tmp || exit
+git checkout HEAD hypr
+cd ..
+rm -rf "$HOME/dotfiles/config/hypr"
+mv tmp/hypr "$HOME/dotfiles/config/"
+rm -rf tmp
+chmod u+x "$HOME/dotfiles/config/hypr/scripts/wsaction.fish"
