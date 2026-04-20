0d1d0f8adf1f52d34bd46afc1c2cc4cfb8124304
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-04-20 09:11:57 +0000

    save

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -3,3 +3,19 @@ require("git"):setup({
 })
 
 require("no-status"):setup()
+
+require("fg"):setup({
+	default_action = "menu", -- nvim, jump
+})
+
+require("eza-preview"):setup({
+	default_tree = true,
+	level = 2,
+	icons = true,
+	follow_symlinks = true,
+	dereference = false,
+	all = true,
+	ignore_glob = { ".git" },
+	git_ignore = true,
+	git_status = true,
+})
