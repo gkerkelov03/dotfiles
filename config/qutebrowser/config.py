5b469847870da1cd753bac18ca055edd8ecb599e
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-05-03 09:04:15 +0000

    save

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -3,27 +3,31 @@ import os
 # Define the global variables that are available so the ide knows and about them and doesn't mark all lines as errors
 c: Keymerger = c  # noqa: F821, E0602
 config: ConfigAPI = config  # noqa: F821, E0602
+allModes = ["command", "insert", "hint", "caret", "passthrough", "prompt", "yesno"]
 
 config.load_autoconfig(False)  # Don't allow config outside of this file
 c.bindings.default = {}  # Unbind all default keybingings
-c.colors.webpage.preferred_color_scheme = "dark"
+c.colors.webpage.preferred_color_scheme = "dark"  # Set dark mode prefference
 
-config.bind(":", "cmd-set-text :")
-config.bind("<Return>", "command-accept", mode="command")
 config.bind("i", "mode-enter insert", mode="normal")
-config.bind("<Ctrl-[>", "mode-leave", mode="insert")
-config.bind("<Ctrl-[>", "mode-leave", mode="command")
-config.bind("o", "cmd-set-text -s :open")
-config.bind("O", "cmd-set-text -s :open -t")
+config.bind(":", "cmd-set-text :", mode="normal")
+config.bind("<Return>", "prompt-accept", mode="prompt")
+config.bind("<Return>", "command-accept", mode="command")
 config.bind("<Up>", "completion-item-focus prev", mode="command")
 config.bind("<Down>", "completion-item-focus next", mode="command")
+for mode in allModes:
+    config.bind("<Ctrl-[>", "mode-leave", mode=mode)
+
+
+config.bind("o", "cmd-set-text -s :open")
+config.bind("O", "cmd-set-text -s :open -t")
 
 # Write your text in in nvim
 c.editor.command = ["foot", "nvim", "{file}"]
 config.bind("<Ctrl-e>", "edit-text", mode="insert")
 config.bind("<Ctrl-e>", "edit-command", mode="command")
 
-# Navigation
+# Scrolling
 config.bind("h", "scroll left")
 config.bind("j", "scroll down")
 config.bind("k", "scroll up")
@@ -32,12 +36,12 @@ config.bind("gg", "scroll-to-perc 0")
 config.bind("G", "scroll-to-perc 100")
 
 # Tab Management
-config.bind("d", "tab-close")  # 'd' is usually tab-close in qutebrowser
-config.bind("u", "undo")  # Restore closed tab
-config.bind("H", "back")
-config.bind("L", "forward")
-config.bind("J", "tab-prev")  # Focus prev tab
-config.bind("K", "tab-next")  # Focus next tab
+config.bind("x", "tab-close")
+config.bind("u", "undo")
+config.bind("J", "back")
+config.bind("K", "forward")
+config.bind("H", "tab-prev")  # Focus prev tab
+config.bind("L", "tab-next")  # Focus next tab
 config.bind("gh", "tab-move -")  # Move tab to the left
 config.bind("gl", "tab-move +")  # Move tab to the right
 config.bind("a", "config-cycle tabs.show always never")
@@ -49,6 +53,9 @@ config.bind("f", "hint")  # Open link in current tab
 config.bind("F", "hint all tab")  # Open link in new tab
 config.bind("cd", "download-clear")  # Close/clear downloads
 config.bind("cn", "clear-messages")  # Close/clear notifications
+config.bind("p", "open -- {clipboard}")
+config.bind("P", "open -t -- {clipboard}")
+config.bind("d", "tab-clone")
 c.messages.timeout = 3000
 
 # Marks & History
@@ -61,7 +68,7 @@ config.bind("sd", "devtools")
 # Yank & URL display
 config.bind("yy", "yank url")  # Copy current url
 config.bind("yf", "hint links yank")  # Copy some url on the page
-config.bind("so", "set-cmd-text -s :open {url:pretty}")  # Show opened url
+config.bind("so", "cmd-set-text -s :open {url:pretty}")  # Show opened url
 
 # Video Speed Controls
 config.bind(
@@ -94,7 +101,12 @@ c.url.searchengines = {
 }
 
 # alias :src to :config-source and so on...
-c.aliases = {"src": "config-source"}
+c.aliases = {
+    "src": "config-source",
+    "inst": "https://instagram.com",
+    "yt": "https://youtube.com",
+    "ytm": "https://music.youtube.com",
+}
 
 # Setup q w e to do -0.1 +0.1 =1 speed for videos
 config.bind(
@@ -126,6 +138,10 @@ c.completion.height = "20%"
 c.tabs.show = "switching"
 c.statusbar.show = "in-mode"
 c.input.insert_mode.auto_leave = False
+config.bind("y", "prompt-accept yes", mode="yesno")
+config.bind("n", "prompt-accept no", mode="yesno")
+config.bind("Y", "prompt-accept always", mode="yesno")
+config.bind("N", "prompt-accept never", mode="yesno")
 
 # Tabs colors
 c.colors.tabs.even.bg = "#282c34"
