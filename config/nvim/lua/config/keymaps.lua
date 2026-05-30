c6dd8dd5a6b15d3dac442349320556f705e80823
Georgi Kerkelov <gkerkelov03@gmail.com>
2026-05-31 00:45:35 +0300

    save

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
-keymap.set({ "n", "x" }, "0", "^")                                -- Map 0 to ^
-keymap.set("n", "o", "o<Esc>")                                    -- Open new lines without staying in insert mode
-keymap.set("n", "<leader>da", ":%d<cr>", { desc = "Delete all" }) -- Delete all
-keymap.set("n", "<leader>ya", ":%y+<cr>", { desc = "Copy all" })  -- Copy all
-keymap.set("n", "P", "kp", { desc = "Paste on line above" })      -- Paste on the line above
+keymap.set({ "n", "x" }, "0", "^") -- Map 0 to ^
+keymap.set("n", "o", "o<Esc>") -- Open new lines without staying in insert mode
+keymap.set("n", "da", ":%d<cr>", { desc = "Delete all" }) -- Delete all
+keymap.set("n", "ya", ":%y+<cr>", { desc = "Copy all" }) -- Copy all
+keymap.set("n", "P", "kp", { desc = "Paste on line above" }) -- Paste on the line above
